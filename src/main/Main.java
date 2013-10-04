4b0dad870a800af9827af72a0bf0584bcc734c33
Croo <croohmanoor@gmail.com>
2013-10-04 19:12:39 +0200

    Added more examples on how to use the different classes created so far. There's also an experimental code which tries to clear up garbage characters and words from the tweets.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,30 +1,44 @@
 package main;
 
 import java.util.List;
-import java.util.Map;
 
-import logic.WordFrequency;
 import twitter4j.Status;
 import database.Database;
 import database.csv.CSVDatabase;
 
 public class Main {
 	 public static void main(String[] args) {
-		 	Database database = new CSVDatabase(args[0]);
+		 	Database database = new CSVDatabase("happy.txt");
 		 	List<Status> tweets = database.getTweets("#happy");
 		 	
-		 	//for(Status tweet: tweets) {
-		 	//	System.out.println( tweet.getUser().getName() + " - " + tweet.getText());
-		 	//}
-		 	
-		 	WordFrequency wf = new WordFrequency(tweets);
-		 	wf.getTotalNumberOfWords();
-		 	Map<String,Integer> wf_map = wf.getWordFrequency();
-		 	
-		 	for(Map.Entry<String, Integer> entry : wf_map.entrySet()) {
-		 		System.out.println(entry.getKey()+"§ "+entry.getValue());
+		 	for(Status tweet: tweets) {
+		 		System.out.println(tweet.getText());
+		 		String message = tweet.getText();
+		 		message = message.replaceAll("@[A-Za-z0-9_]*","");
+		 		message = message.replaceAll("#","");
+		 		message = message.replaceAll("(http://.*[.].*/.*[ ]|http://.*[.].*/.*$)","");
+		 		message = message.replaceAll("[ ]+"," ");
+		 		
+		 		System.out.println(message);
+		 		System.out.println("");
 		 	}
 		 	
-		 	
+//		 	WordFrequency wf = new WordFrequency(tweets);
+//		 	wf.getTotalNumberOfWords();
+//		 	Map<String,Integer> wf_map = wf.getWordFrequency();
+//		 	
+//		 	for(Map.Entry<String, Integer> entry : wf_map.entrySet()) {
+//		 		System.out.println(entry.getKey()+ args[1] + " "+entry.getValue() + "\r");
+//		 	}
+		 
+		 
+		 
+//		 	SentiWordNet s = SentiWordNet.getInstance();
+//		 	System.out.println("Sad: " + s.getWeight("sad"));
+//		 	System.out.println("Happy: " + s.getWeight("happy"));
+//		 	System.out.println("cake: " + s.getWeight("cake"));
+//		 	System.out.println("orange: " + s.getWeight("orange"));
+//		 	System.out.println("disaster: " + s.getWeight("disaster"));
+//		 	System.out.println("earthquake: " + s.getWeight("earthquake"));
 	    }
 }

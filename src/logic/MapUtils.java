6c4dd74ca5b2ad0cd3b6eb45e9488d02cae49bd7
Croo <croohmanoor@gmail.com>
2013-10-04 10:28:34 +0200

    moved classes doing the wordFrequncy thing to a separate package named logic.

--- src/logic/MapUtils.java
+++ src/logic/MapUtils.java
@@ -1,4 +1,4 @@
-package main;
+package logic;
 
 import java.util.Collections;
 import java.util.Comparator;
--- src/logic/WordFrequency.java
+++ src/logic/WordFrequency.java
@@ -1,4 +1,4 @@
-package main;
+package logic;
 
 import java.util.HashMap;
 import java.util.List;

b6c3b0cad58cd4a40b3207758d1615a990e86262
Croo <croohmanoor@gmail.com>
2013-10-22 17:10:57 +0200

    Equal function for Word. It returns true if the string value of he two Word is the same.

--- src/logic/Word.java
+++ src/logic/Word.java
@@ -49,4 +49,29 @@ public class Word {
 	public void setNegativeWeight(Double average) {
 		negativeWeight = average;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((word == null) ? 0 : word.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Word other = (Word) obj;
+		if (word == null) {
+			if (other.word != null)
+				return false;
+		} else if (!word.equals(other.word))
+			return false;
+		return true;
+	}
 }

6553a0c1d294096622f848f8b09a668541b20cb1
Croo <croohmanoor@gmail.com>
2013-10-22 14:54:57 +0200

    Removed some junk left over from the implementation-then-deletion of Jazzy spellchecker. Because it was slow as ... something very slow.

--- src/logic/TweetAnalyzer.java
+++ src/logic/TweetAnalyzer.java
@@ -1,19 +1,12 @@
 package logic;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import twitter4j.Status;
-
-import com.swabunga.spell.engine.SpellDictionaryHashMap;
-
 import database.sentimental.BoostWords;
 import database.sentimental.Emoticons;
 import database.sentimental.SentiWordNet;
@@ -27,13 +20,6 @@ public class TweetAnalyzer {
 	private SentiWordNet sentiWordNet;
 	private Emoticons emoticons;
 	private BoostWords boostWords;
-	
-	/*
-	 * This is a datastructure needed for Jazzy spell checker to calculate crazy things.
-	 * The Jazzy uses it somewhere deep in the code and resizes it as needed, but it is much more efficent to
-	 * create it here. 
-	 */
-	private int[][] spellcheckMatrix = new int[0][0];
 	
 	public static int numberOfUnknownWords = 0;
 

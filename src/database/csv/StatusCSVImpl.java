cc94a8d9f81eec43831ca9998737d8178f47834c
Croo <croohmanoor@gmail.com>
2013-11-07 03:01:09 +0100

    added hashcode & equals on string.

--- src/database/csv/StatusCSVImpl.java
+++ src/database/csv/StatusCSVImpl.java
@@ -15,6 +15,31 @@ import twitter4j.UserMentionEntity;
 
 public class StatusCSVImpl implements Status {
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StatusCSVImpl other = (StatusCSVImpl) obj;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		return true;
+	}
+
 	private static final long serialVersionUID = 1L;
 	private Date createdAt;
 	private String text;
